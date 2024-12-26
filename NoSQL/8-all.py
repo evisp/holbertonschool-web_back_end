0fdf5aa4e1666f460fbf5dc16854b85fa325b5bf
evisp <evisplaku@gmail.com>
2024-12-26 08:53:04 +0000

    add NoSQL tasks

---
+++ NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A python module that manitpulates mongo DB"""
+
+
+def list_all(mongo_collection):
+    """A function that retrieves all elements of
+    a mongoDB given collection. If no document in the collection
+    returns an empty list"""
+
+    return [doc for doc in mongo_collection.find()]
